f23fd27a54d1345bf96f5a5706a2c3abf61f95bb
kaanakdik <kaanakdik@gmail.com>
2025-09-12 18:06:21 +0300

    docker eklendi

---
+++ init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS messages (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100),
+    message TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
